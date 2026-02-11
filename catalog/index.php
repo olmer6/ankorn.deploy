872bce883416d5e488ac6e41e14effb862836ec2
Вячеслав Климов <olmer6@yandex.ru>
2026-02-11 14:26:05 +0700

    увеличиваем видимую глубину вложенности каталога

--- catalog/index.php
+++ catalog/index.php
@@ -67,7 +67,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 		],
 		"SHOW_TOP_ELEMENTS" => "N",
 		"SECTION_COUNT_ELEMENTS" => "N",
-		"SECTION_TOP_DEPTH" => "1",
+		"SECTION_TOP_DEPTH" => "5",
 		"SECTIONS_VIEW_MODE" => "TILE",
 		"SECTIONS_SHOW_PARENT_NAME" => "N",
 		"PAGE_ELEMENT_COUNT" => "15",
