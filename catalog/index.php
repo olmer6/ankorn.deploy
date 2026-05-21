570cb754f181e9f4ec23ae51a33967f0eb4443c7
Вячеслав Климов <olmer6@yandex.ru>
2026-05-21 17:51:48 +0700

    Убрали "Каталог продукции" на странице карточки товара

--- catalog/index.php
+++ catalog/index.php
@@ -3,7 +3,7 @@ define("HIDE_SIDEBAR", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 ?>
 <?php
-	if ( ($APPLICATION->GetCurPage() == '/catalog/') ||  ($APPLICATION->GetCurPage(false) == '/catalog/index.php') || ($_GET['error']==404))  {
+	if ( ($APPLICATION->GetCurPage() == '/catalog/') ||  ($APPLICATION->GetCurPage(false) == '/catalog/index.php'))  {
 	?>
 	<div id="parentCatalog">
 		<div class="page-container">
@@ -344,7 +344,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 	false
 );?>
 <?php
-if ( ($APPLICATION->GetCurPage() == '/catalog/') ||  ($APPLICATION->GetCurPage(false) == '/catalog/index.php')  || ($_GET['error']==404))  {
+if ( ($APPLICATION->GetCurPage() == '/catalog/') ||  ($APPLICATION->GetCurPage(false) == '/catalog/index.php'))  {
 	echo "</div></div>";
 }
 ?>
